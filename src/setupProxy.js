ddf657177bcc4922b89e7dac2bcaa0e31247d632
MegamindsITServices <megamindsresearch@gmail.com>
2024-05-25 01:08:49 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://arrowpublications.onrender.com/",
+      target: "https://new-arrow-backend.onrender.com/",
       changeOrigin: true,
     })
   );
