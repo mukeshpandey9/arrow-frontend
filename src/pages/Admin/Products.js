fb173af684d23c806171bac1136da823b9ebd869
MegamindsITServices <megamindsresearch@gmail.com>
2024-05-27 20:30:37 +0530

    Fix: Responsiveness in Product page

--- src/pages/Admin/Products.js
+++ src/pages/Admin/Products.js
@@ -66,7 +66,7 @@ const Products = () => {
           <div className="col-md-3">
             <AdminMenu />
           </div>
-          <div className="col-md-9">
+          <div className="col-md-9 product-grid">
             <h1 className="text-center">All Products List</h1>
             <div className="d-flex justify-content-center mb-4">
               {/* <SearchInput /> */}
@@ -74,7 +74,7 @@ const Products = () => {
             </div>
             <div className="row">
               {products?.map((p, index) => (
-                <div key={p._id} className="col-md-2">
+                <div key={p._id} className="col-sm-6 col-lg-3 col-xxl-2">
                   <div className="card-6 mb-3">
                     <img
                       src={`/api/v1/product/product-photo/${p._id}`}
@@ -101,7 +101,7 @@ const Products = () => {
 
                   <button
                     className="Butn ms-2"
-                    onClick={ () => {
+                    onClick={async () => {
                       handleDelete(p._id);
                     }}
                   >
